70a9279cb1a4dd768899e6f0887dafcbb995a434
Alex LI <zhi.li@datapebbles.com>
2023-11-20 12:21:29 +0100

    update

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import pyspark
 import pandas as pd
 
-# def do_something():
-#     print('This is V1.0.1')
+def do_something():
+    print('This is V1.0.1')
 
 def main():
     print('This is a demo for Version Control')
